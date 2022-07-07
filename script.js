e74805550c6de513d1fd3a7372bbb463907e78a1
unknown <>
2022-07-07 20:59:48 -0300

    add

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const response = fetch('https://jsonplaceholder.typicode.com/posts/20')
-    .then(res => {
 
-        console.log(res.jason())
+const response = fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+        console.log(response);
+        return response.json();
     })
+
+    .then(data => {
+        console.log(data[0])
+        data.map(user => {
+            console.log(`Nome: ${user.name}`)
+        })
+    })
+
+
 
     .catch(error => {
         console.log(`Deu erro na busca: ${error}`)
